fa548e79082d888106ec4f722e0b6241ac99df6c
Alexandru Nastase <nastase.alexandru100@gmail.com>
2024-02-18 17:38:18 +0000

    Type Annotation Basics

--- Section 03 Type Annotation Basics/variables.ts
+++ Section 03 Type Annotation Basics/variables.ts
@@ -1,28 +1,61 @@
+// --- Basics of Types ---
+
 let movieTitle: string = "Amadeus";
+movieTitle = "Arrival";
+// movieTitle = 9; // TS is gonna complain
+// movieTitle.upper(); // TS will complain
+movieTitle.toUpperCase() // ok
+
 let numCatLives: number = 9;
+numCatLives += 1;
+// numCatLives = "zero" // TS is gonna complain
+
 let gameOver: boolean = false;
+gameOver = true;
+// gameOver = "true" // TS is gonna complain
 
-// Type Inference
-let tvShow = "TC Show Name"; //automatically inferred to string
-tvShow = "Other TV Show";
-// tvShow = 3; //Can't do this
+// --- Type Inference ---
 
-// The Any type
+let tvShow = "Olive Kitteridge"; // TS infers the type of string
+tvShow = "The Other Two"; // ok
+// tvShow = false; // TS is gonna complain
+
+let isFunny = true; // TS infers the type of boolean
+isFunny = false;
+// isFunny = "string" // TS is gonna complain
+
+// --- The any Type ---
+
 let thing: any = "hello";
 thing = 1;
 thing = false;
 thing();
-thing.getFunction();
-// Defeats the purpose of TS and types so use sparingly
+thing.toUpperCase(); // TS won't complain about any of these
 
-// Delayed initialization
+// --- Delayed Initialisation 
+
 const movies = ["Arrival", "The Thing", "Aliens", "Amadeus"];
-let foundMovie: boolean;
+let foundMovie; // If we don't specify the type, TS will assume ANY
 
 for (let movie of movies) {
     if (movie === "Amadeus") {
-        foundMovie = true;
+        foundMovie = "Amadeus";
+        foundMovie();
+        foundMovie = 1;
+        foundMovie.agshbAsngow(); // TS won't complain
     }
 }
 
-// foundMovie = "Some string, even though it should be boolean";
+let foundMovieString: string; // Try to always specify the type
+
+for (let movie of movies) {
+    if (movie === "Amadeus") {
+        foundMovieString = "Amadeus";
+        // foundMovieString();
+        // foundMovieString = 1;
+        // foundMovieString.agshbAsngow(); // TS is gonna complain
+    }
+}
+
+
+// Compilation: use command tsc variables.ts
